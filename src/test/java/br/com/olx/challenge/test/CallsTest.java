ee81ac6a0cbd32490dddf6548a4cbe43d969fe66
Robson <robson.rocha@gmail.com>
2016-11-27 19:52:19 -0200

    Inserção de mais testes.

--- src/test/java/br/com/olx/challenge/test/CallsTest.java
+++ src/test/java/br/com/olx/challenge/test/CallsTest.java
@@ -4,8 +4,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import br.com.olx.challenge.bean.LoginBean;
@@ -16,10 +22,30 @@ import br.com.olx.challenge.test.mock.MainMock;
 import br.com.olx.challenge.test.util.Utils;
 
 public class CallsTest {
-
+	
+	private String baseUrl;
+	private WebDriver driver;
+	
 	private List<Calls> calls = new ArrayList<Calls>();
 	private List<UserLogin> loggedUsers = new ArrayList<UserLogin>();
 	
+	@BeforeClass
+	public void openBrowser() {
+		baseUrl = System.getProperty("webdriver.base.url");
+		if (baseUrl == null || baseUrl.trim().isEmpty())
+			baseUrl = "http://localhost:8080/sacweb";
+		String pathChromeDriver = System.getProperty("webdriver.chrome.driver");
+		if(pathChromeDriver == null || pathChromeDriver.trim().isEmpty())
+			System.setProperty("webdriver.chrome.driver",
+				"/Projetos/primefaces/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+	}
+	
+	@AfterClass
+	public void closeBrowser() {
+		driver.close();
+	}
 
 	@AfterMethod
 	public void delete() {
@@ -170,6 +196,320 @@ public class CallsTest {
 
 	}
 	
+	@Test
+	public void insertWithAllFieldsNullTest() throws InterruptedException{
+		MainMock mm = new MainMock();
+
+		String strLogin = "challenge1";
+		String strPassword = Utils.randomPassword();
+
+		UserLogin loggedUser = mm.createUserLogin(strLogin, strPassword, false);
+		this.loggedUsers.add(loggedUser);
+		
+		WebElement login = driver.findElement(By.id("frmLogin:login"));
+		login.sendKeys(strLogin);
+		WebElement password = driver.findElement(By.id("frmLogin:password"));
+		password.sendKeys(strPassword);		
+		WebElement button = driver.findElement(By.id("frmLogin:doLogin"));
+		button.click();
+		Thread.sleep(3000);
+		WebElement btnMenu = driver.findElement(By
+				.className("ui-button-text"));
+		Assert.assertTrue(btnMenu.getText().equals(strLogin));
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnInsert = driver.findElement(By.id("main:mnuInsert"));
+		btnInsert.click();
+		WebElement btnInsertCall = driver.findElement(By.id("formInsert:btnInsert"));
+		btnInsertCall.click();
+		Thread.sleep(1500);
+		List<WebElement> errors = driver.findElements(By
+				.className("ui-growl-title"));
+		Assert.assertTrue(errors.get(0).getText().equals("Escolha o tipo do chamado."));
+		Assert.assertTrue(errors.get(1).getText().equals("Escolha o motivo."));
+		Assert.assertTrue(errors.get(2).getText().equals("Escolha o Estado."));
+		Assert.assertTrue(errors.get(3).getText().equals("Preencha o campo Descrição."));
+		
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnLogout = driver.findElement(By.id("main:mnuExit"));
+		btnLogout.click();
+		Thread.sleep(3000);
+		WebElement login2 = driver.findElement(By.id("frmLogin:login"));
+		Assert.assertTrue(login2 != null);
+	}
+	
+	@Test
+	public void insertWithoutTypeTest() throws InterruptedException{
+		MainMock mm = new MainMock();
+
+		String strLogin = "challenge1";
+		String strPassword = Utils.randomPassword();
+
+		UserLogin loggedUser = mm.createUserLogin(strLogin, strPassword, false);
+		this.loggedUsers.add(loggedUser);
+		
+		WebElement login = driver.findElement(By.id("frmLogin:login"));
+		login.sendKeys(strLogin);
+		WebElement password = driver.findElement(By.id("frmLogin:password"));
+		password.sendKeys(strPassword);		
+		WebElement button = driver.findElement(By.id("frmLogin:doLogin"));
+		button.click();
+		Thread.sleep(3000);
+		WebElement btnMenu = driver.findElement(By
+				.className("ui-button-text"));
+		Assert.assertTrue(btnMenu.getText().equals(strLogin));
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnInsert = driver.findElement(By.id("main:mnuInsert"));
+		btnInsert.click();
+		
+		List<WebElement> reasons = driver.findElements(By.id("formInsert:reason"));
+		reasons.get(0).click();
+		
+		WebElement state = driver.findElement(By.id("formInsert:state"));
+		state.sendKeys("RJ");
+		
+		WebElement description = driver.findElement(By.id("formInsert:description"));
+		description.sendKeys("Teste Unitário.");
+		
+		WebElement btnInsertCall = driver.findElement(By.id("formInsert:btnInsert"));
+		btnInsertCall.click();
+		Thread.sleep(3000);
+		List<WebElement> errors = driver.findElements(By
+				.className("ui-growl-title"));
+		Assert.assertTrue(errors.get(0).getText().equals("Escolha o tipo do chamado."));
+		
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnLogout = driver.findElement(By.id("main:mnuExit"));
+		btnLogout.click();
+		Thread.sleep(3000);
+		WebElement login2 = driver.findElement(By.id("frmLogin:login"));
+		Assert.assertTrue(login2 != null);
+	}
+	
+	@Test
+	public void insertWithoutReasonTest() throws InterruptedException{
+		MainMock mm = new MainMock();
+
+		String strLogin = "challenge1";
+		String strPassword = Utils.randomPassword();
+
+		UserLogin loggedUser = mm.createUserLogin(strLogin, strPassword, false);
+		this.loggedUsers.add(loggedUser);
+		
+		WebElement login = driver.findElement(By.id("frmLogin:login"));
+		login.sendKeys(strLogin);
+		WebElement password = driver.findElement(By.id("frmLogin:password"));
+		password.sendKeys(strPassword);		
+		WebElement button = driver.findElement(By.id("frmLogin:doLogin"));
+		button.click();
+		Thread.sleep(3000);
+		WebElement btnMenu = driver.findElement(By
+				.className("ui-button-text"));
+		Assert.assertTrue(btnMenu.getText().equals(strLogin));
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnInsert = driver.findElement(By.id("main:mnuInsert"));
+		btnInsert.click();
+		
+		List<WebElement> types = driver.findElements(By.id("formInsert:typeCall"));
+		types.get(0).click();
+		
+		WebElement state = driver.findElement(By.id("formInsert:state"));
+		state.sendKeys("RJ");
+		
+		WebElement description = driver.findElement(By.id("formInsert:description"));
+		description.sendKeys("Teste Unitário.");
+		
+		WebElement btnInsertCall = driver.findElement(By.id("formInsert:btnInsert"));
+		btnInsertCall.click();
+		Thread.sleep(3000);
+		List<WebElement> errors = driver.findElements(By
+				.className("ui-growl-title"));
+		Assert.assertTrue(errors.get(0).getText().equals("Escolha o motivo."));
+		
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnLogout = driver.findElement(By.id("main:mnuExit"));
+		btnLogout.click();
+		Thread.sleep(3000);
+		WebElement login2 = driver.findElement(By.id("frmLogin:login"));
+		Assert.assertTrue(login2 != null);
+	}
+	
+	@Test
+	public void insertWithoutStateTest() throws InterruptedException{
+		MainMock mm = new MainMock();
+
+		String strLogin = "challenge1";
+		String strPassword = Utils.randomPassword();
+
+		UserLogin loggedUser = mm.createUserLogin(strLogin, strPassword, false);
+		this.loggedUsers.add(loggedUser);
+		
+		WebElement login = driver.findElement(By.id("frmLogin:login"));
+		login.sendKeys(strLogin);
+		WebElement password = driver.findElement(By.id("frmLogin:password"));
+		password.sendKeys(strPassword);		
+		WebElement button = driver.findElement(By.id("frmLogin:doLogin"));
+		button.click();
+		Thread.sleep(3000);
+		WebElement btnMenu = driver.findElement(By
+				.className("ui-button-text"));
+		Assert.assertTrue(btnMenu.getText().equals(strLogin));
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnInsert = driver.findElement(By.id("main:mnuInsert"));
+		btnInsert.click();
+		
+		List<WebElement> reasons = driver.findElements(By.id("formInsert:reason"));
+		reasons.get(0).click();
+		
+		List<WebElement> types = driver.findElements(By.id("formInsert:typeCall"));
+		types.get(0).click();
+		
+		WebElement description = driver.findElement(By.id("formInsert:description"));
+		description.sendKeys("Teste Unitário.");
+		
+		WebElement btnInsertCall = driver.findElement(By.id("formInsert:btnInsert"));
+		btnInsertCall.click();
+		Thread.sleep(3000);
+		List<WebElement> errors = driver.findElements(By
+				.className("ui-growl-title"));
+		Assert.assertTrue(errors.get(0).getText().equals("Escolha o Estado."));
+		
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnLogout = driver.findElement(By.id("main:mnuExit"));
+		btnLogout.click();
+		Thread.sleep(3000);
+		WebElement login2 = driver.findElement(By.id("frmLogin:login"));
+		Assert.assertTrue(login2 != null);
+	}
+	
+	@Test
+	public void insertWithoutDescriptionTest() throws InterruptedException{
+		MainMock mm = new MainMock();
+
+		String strLogin = "challenge1";
+		String strPassword = Utils.randomPassword();
+
+		UserLogin loggedUser = mm.createUserLogin(strLogin, strPassword, false);
+		this.loggedUsers.add(loggedUser);
+		
+		WebElement login = driver.findElement(By.id("frmLogin:login"));
+		login.sendKeys(strLogin);
+		WebElement password = driver.findElement(By.id("frmLogin:password"));
+		password.sendKeys(strPassword);		
+		WebElement button = driver.findElement(By.id("frmLogin:doLogin"));
+		button.click();
+		Thread.sleep(3000);
+		WebElement btnMenu = driver.findElement(By
+				.className("ui-button-text"));
+		Assert.assertTrue(btnMenu.getText().equals(strLogin));
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnInsert = driver.findElement(By.id("main:mnuInsert"));
+		btnInsert.click();
+		
+		List<WebElement> reasons = driver.findElements(By.id("formInsert:reason"));
+		reasons.get(0).click();
+		
+		List<WebElement> types = driver.findElements(By.id("formInsert:typeCall"));
+		types.get(0).click();
+		
+		WebElement state = driver.findElement(By.id("formInsert:state"));
+		state.sendKeys("RJ");
+		
+		WebElement btnInsertCall = driver.findElement(By.id("formInsert:btnInsert"));
+		btnInsertCall.click();
+		Thread.sleep(3000);
+		List<WebElement> errors = driver.findElements(By
+				.className("ui-growl-title"));
+		Assert.assertTrue(errors.get(0).getText().equals("Preencha o campo Descrição."));
+		
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnLogout = driver.findElement(By.id("main:mnuExit"));
+		btnLogout.click();
+		Thread.sleep(3000);
+		WebElement login2 = driver.findElement(By.id("frmLogin:login"));
+		Assert.assertTrue(login2 != null);
+	}
+	
+	@Test
+	public void correctInsertTest() throws InterruptedException{
+		MainMock mm = new MainMock();
+
+		String strLogin = "challenge1";
+		String strPassword = Utils.randomPassword();
+
+		UserLogin loggedUser = mm.createUserLogin(strLogin, strPassword, false);
+		this.loggedUsers.add(loggedUser);
+		
+		WebElement login = driver.findElement(By.id("frmLogin:login"));
+		login.sendKeys(strLogin);
+		WebElement password = driver.findElement(By.id("frmLogin:password"));
+		password.sendKeys(strPassword);		
+		WebElement button = driver.findElement(By.id("frmLogin:doLogin"));
+		button.click();
+		Thread.sleep(3000);
+		WebElement btnMenu = driver.findElement(By
+				.className("ui-button-text"));
+		Assert.assertTrue(btnMenu.getText().equals(strLogin));
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnInsert = driver.findElement(By.id("main:mnuInsert"));
+		btnInsert.click();
+		
+		List<WebElement> reasons = driver.findElements(By.id("formInsert:reason"));
+		reasons.get(0).click();
+		
+		List<WebElement> types = driver.findElements(By.id("formInsert:typeCall"));
+		types.get(0).click();
+		
+		WebElement state = driver.findElement(By.id("formInsert:state"));
+		state.sendKeys("RJ");
+		
+		WebElement description = driver.findElement(By.id("formInsert:description"));
+		description.sendKeys("Teste Unitário.");
+		
+		WebElement btnInsertCall = driver.findElement(By.id("formInsert:btnInsert"));
+		btnInsertCall.click();
+		Thread.sleep(3000);
+				
+		btnMenu = driver.findElement(By.name("main:splMenu_menuButton"));
+		btnMenu.click();
+		WebElement btnLogout = driver.findElement(By.id("main:mnuExit"));
+		btnLogout.click();
+		Thread.sleep(3000);
+		WebElement login2 = driver.findElement(By.id("frmLogin:login"));
+		Assert.assertTrue(login2 != null);
+		
+		LoginBean lb = new LoginBean();
+
+		lb.setLogin(loggedUser.getLogin());
+		lb.setPassword(loggedUser.getPassword());
+		lb.setNavigation(new RedirectBean());
+		lb.doLogin();
+
+		Assert.assertTrue(lb.getLoggedUser() != null);
+
+		mm.setLoginBean(lb);
+
+		List<Calls> list = mm.getCalls();
+
+		Calls insertedCall = list.get(0);
+		
+		Assert.assertTrue(insertedCall.getDescription().equals("Teste Unitário."));
+		Assert.assertTrue(insertedCall.getUser().getLogin().equals(loggedUser.getLogin()));
+		
+		calls.add(insertedCall);		
+		
+	}
+	
 	@Test(expectedExceptions=SQLException.class)
 	public void insertCallWithWrongTypeTest() throws SQLException {
 		MainMock mm = new MainMock();
@@ -241,7 +581,6 @@ public class CallsTest {
 		c.setDescription("Teste");
 		c.setReason('S');
 		c.setState("RJ");
-		//c.setTypeCall('X');
 		
 		mm.insertCall(c);
 
@@ -257,7 +596,7 @@ public class CallsTest {
 		Assert.assertTrue(insertedCall.getState().equals(c.getState()));
 		Assert.assertTrue(insertedCall.getTypeCall() == c.getTypeCall());
 		Assert.assertTrue(insertedCall.getUser().equals(loggedUser));
-
+		
 	}
 	
 	@Test(expectedExceptions=SQLException.class)
@@ -329,7 +668,6 @@ public class CallsTest {
 		Calls c = new Calls();
 
 		c.setDescription("Teste");
-		//c.setReason('S');
 		c.setState("RJ");
 		c.setTypeCall('C');
 		
@@ -375,7 +713,6 @@ public class CallsTest {
 
 		c.setDescription("Teste");
 		c.setReason('S');
-		//c.setState("RJ");
 		c.setTypeCall('C');
 		
 		mm.insertCall(c);
@@ -418,7 +755,6 @@ public class CallsTest {
 
 		Calls c = new Calls();
 
-		//c.setDescription("Teste");
 		c.setReason('S');
 		c.setState("RJ");
 		c.setTypeCall('C');
